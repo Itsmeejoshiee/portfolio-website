ad4134ced01f8d75f36947ac1e46422986c52361
itsmeejoshiee <joshgorospe03@gmail.com>
2025-05-22 18:49:42 +0800

    Add: business logic on projects

--- lib/sections/projects/widgets/projects_card.dart
+++ lib/sections/projects/widgets/projects_card.dart
@@ -6,6 +6,7 @@ class ProjectsCard extends StatelessWidget {
   final String appName;
   final String appDescription;
   final Color appColor;
+  final void Function()? onTap;
 
   const ProjectsCard({
     super.key,
@@ -13,52 +14,56 @@ class ProjectsCard extends StatelessWidget {
     required this.appName,
     required this.appColor,
     required this.appDescription,
+    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 300,
-      width: 550,
-      child: Card(
-        color: appColor,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: Row(
-            children: [
-              Flexible(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(appName, style: poppinsH3.copyWith(fontSize: 30)),
-                    SizedBox(height: 10),
-                    Text(
-                      appDescription,
-                      style: poppinsBody,
-                      maxLines: 4,
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.start,
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                width: 260,
-                child: Stack(
-                  children: [
-                    Positioned(
-                      top: 15,
-                      child: SizedBox(
-                        width: 260,
-                        child: Image.asset(imagePath, fit: BoxFit.cover),
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        height: 300,
+        width: 550,
+        child: Card(
+          color: appColor,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              children: [
+                Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(appName, style: poppinsH3.copyWith(fontSize: 30)),
+                      SizedBox(height: 10),
+                      Text(
+                        appDescription,
+                        style: poppinsBody,
+                        maxLines: 4,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.start,
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  width: 260,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 15,
+                        child: SizedBox(
+                          width: 260,
+                          child: Image.asset(imagePath, fit: BoxFit.cover),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
