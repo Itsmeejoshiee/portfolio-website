62a69454cb51695ae341fb49dd4c820d9582288b
itsmeejoshiee <joshgorospe03@gmail.com>
2025-05-27 03:00:47 +0800

    fix: responsiveness of projects_card

--- lib/sections/projects/widgets/projects_card.dart
+++ lib/sections/projects/widgets/projects_card.dart
@@ -20,7 +20,6 @@ class ProjectsCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
     return MouseRegion(
       cursor: SystemMouseCursors.click,
       child: GestureDetector(
